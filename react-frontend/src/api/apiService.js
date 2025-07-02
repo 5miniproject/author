567ee84d09070a4cdd23f054a44e1cc1b5f7333a
Kim JaeHong <asdr111324@gmail.com>
2025-07-02 06:12:22 +0000

    프론트-dockerfile,api경로수정

--- react-frontend/src/api/apiService.js
+++ react-frontend/src/api/apiService.js
@@ -1,8 +1,11 @@
 // frontend/src/api/apiService.js
 import axios from 'axios';
 
-// const API_BASE_URL = 'http://localhost:8088/'; // 백엔드 API Gateway URL
-const API_BASE_URL = 'https://8088-5miniprojec-libraryproj-19bfadrkwwl.ws-us120.gitpod.io';
+// const API_BASE_URL = 'https://8088-5miniprojec-libraryproj-19bfadrkwwl.ws-us120.gitpod.io';
+// 백엔드 API Gateway Service의 이름은 'gateway'이고 포트는 '8080'입니다.
+// 같은 네임스페이스에 프론트엔드와 백엔드가 배포된다고 가정합니다.
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8088/';
+
 
 const apiService = axios.create({
     baseURL: API_BASE_URL,
