828f95f68bca36ef2afb5beb0ee75a6956e861bd
Kim JaeHong <asdr111324@gmail.com>
2025-07-03 13:49:19 +0000

    init.sh에 helm추가,fronend 폴더삭제

--- init.sh
+++ init.sh
@@ -9,6 +9,10 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ jammy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
 sudo apt update
 sudo apt install azure-cli
 
@@ -20,4 +24,3 @@ export NODE_OPTIONS=--openssl-legacy-provider
 
 cd infra
 docker-compose up
-sdk install java
